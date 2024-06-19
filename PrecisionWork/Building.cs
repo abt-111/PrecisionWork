2108fcfebd185894ef6ec8fab596b3ad35171663
abt93 <anthony.temmel@gmail.com>
2024-06-19 14:22:25 +0200

    Ajout de la classe building, de ses propriétés et méthodes

---
+++ PrecisionWork/Building.cs
@@ -0,0 +1,31 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace PrecisionWork
+{
+    internal class Building
+    {
+        private uint _size;
+        private uint _floorCount;
+
+        public uint GetFloorCount() => _floorCount;
+
+        public uint GetSize() => _size;
+
+        public Building(uint height, uint floorCount)
+        {
+            _size = height;
+            _floorCount = floorCount;
+        }
+
+        public Building(uint floorCount)
+        {
+            _size = 3 * floorCount;
+            _floorCount = floorCount;
+        }
+
+        public float GetFloorMaxSize()
+        {
+            return _size / _floorCount;
+        }
+    }
+}
